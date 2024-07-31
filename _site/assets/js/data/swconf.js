3a93d8a500bf41e2ee7f0862691fec5c5cb57c36
ivy <ivyng@seas.upenn.edu>
2024-07-31 14:45:03 -0400

    added starter files

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,25 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1722049060',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+
+      
+      
+        '/assets/js/dist/app.min.js',
+        '/assets/js/dist/categories.min.js',
+        '/assets/js/dist/commons.min.js',
+        '/assets/js/dist/home.min.js',
+        '/assets/js/dist/misc.min.js',
+        '/assets/js/dist/page.min.js',
+        '/assets/js/dist/post.min.js'
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
